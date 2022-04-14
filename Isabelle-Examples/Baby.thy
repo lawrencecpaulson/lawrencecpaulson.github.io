c8b68bb064790ae494989e25b1ef6343ea3a1f6c
paulson <lp15@cam.ac.uk>
2022-04-14 17:09:35 +0100

    Starting to write a baby example for 4 May.

---
+++ Isabelle-Examples/Baby.thy
@@ -0,0 +1,62 @@
+section \<open>Baby examples\<close>
+
+theory Baby imports
+  "HOL-Decision_Procs.Approximation"
+
+begin
+
+text \<open>Kevin Buzzard's examples\<close>
+lemma
+  fixes x::real
+  shows "(x+y)*(x+2*y)*(x+3*y) = x^3 + 6*x^2*y + 11*x*y^2 + 6*y^3"
+  by (simp add: algebra_simps eval_nat_numeral)
+
+lemma "sqrt 2 + sqrt 3 < sqrt 10"
+proof -
+  have "(sqrt 2 + sqrt 3)^2 < (sqrt 10)^2"
+  proof (simp add: algebra_simps eval_nat_numeral)
+    have "(2 * (sqrt 2 * sqrt 3))^2 < 5 ^ 2"
+      by (simp add: algebra_simps eval_nat_numeral)
+    then show "2 * (sqrt 2 * sqrt 3) < 5"
+      by (smt (verit, best) power_mono)
+  qed
+  then show ?thesis
+    by (simp add: real_less_rsqrt)
+qed
+
+lemma "sqrt 2 + sqrt 3 < sqrt 10"
+  by (approximation 10)
+
+lemma "3.141592635389 < pi"
+  by (approximation 30)
+
+lemma
+  fixes a::real
+  shows "(a*b + b * c + c*a)^3 \<le> (a^2 + a * b + b^2) * (b^2 + b * c + c^2) * (c^2 + c*a + a^2)"
+  by sos
+
+lemma "sqrt 2 \<notin> \<rat>"
+proof
+  assume "sqrt 2 \<in> \<rat>"
+  then obtain q::rat where "sqrt 2 = of_rat q"
+    using Rats_cases by blast
+  then have "q^2 = 2"
+    by (metis abs_numeral of_rat_eq_iff of_rat_numeral_eq of_rat_power power2_eq_square real_sqrt_mult_self)
+  then obtain m n
+    where "coprime m n" "q = of_int m / of_int n"
+    by (metis Fract_of_int_quotient Rat_cases)
+  then have "of_int m ^ 2 / of_int n ^ 2 = (2::rat)"
+    by (metis \<open>q\<^sup>2 = 2\<close> power_divide)
+  then have 2: "of_int m ^ 2 = (2::rat) * of_int n ^ 2"
+    by (metis division_ring_divide_zero double_eq_0_iff mult_2_right mult_zero_right nonzero_divide_eq_eq)
+  then have "2 dvd m"
+    by (metis (mono_tags, lifting) even_mult_iff even_numeral of_int_eq_iff of_int_mult of_int_numeral power2_eq_square)
+  then obtain r where "m = 2*r"
+    by blast
+  then have "2 dvd n"
+    by (smt (verit) "2" \<open>even m\<close> dvdE even_mult_iff mult.left_commute mult_cancel_left of_int_1 of_int_add of_int_eq_iff of_int_mult one_add_one power2_eq_square)
+  then show False
+    using \<open>coprime m n\<close> \<open>m = 2 * r\<close> by simp
+qed
+
+end
---
+++ _drafts/2022-05-04-baby-examples.md
@@ -0,0 +1,42 @@
+---
+layout: post
+title:  "Getting started: some baby examples"
+usemathjax: true 
+tags: examples, Isabelle
+---
+
+For absolute beginners, proof assistants are daunting. Everything you do seems to go wrong. So let's have some super simple examples that should be easy to follow, and that already highlight some pitfalls to watch out for.
+
+
+
+<pre class="source">
+</pre>
+
+
+
+
+
+
+<pre class="source">
+</pre>
+
+
+
+
+
+<pre class="source">
+</pre>
+
+
+
+
+
+<pre class="source">
+</pre>
+
+
+
+
+To run this example yourself, you need to install [Isabelle](https://isabelle.in.tum.de/) (it's easy!) and perhaps read some [introductory documentation](https://isabelle.in.tum.de/dist/Isabelle/doc/prog-prove.pdf). 
+
+The file containing this example can be downloaded [here](/Isabelle-Examples/Fibonacci.thy).
